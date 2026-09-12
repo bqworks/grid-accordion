a58937961735191dc4f729b1735531f70c6c5ab6
David Ghiurau <contact@bqworks.net>
2026-09-12 12:25:18 +0300

    update plugin version

--- grid-accordion.php
+++ grid-accordion.php
@@ -4,7 +4,7 @@
 	Plugin Name: Grid Accordion
 	Plugin URI:  https://bqworks.net/grid-accordion/
 	Description: Responsive and touch-enabled grid accordion.
-	Version:     1.9.15
+	Version:     1.9.16
 	Author:      bqworks
 	Author URI:  https://bqworks.net
 */
